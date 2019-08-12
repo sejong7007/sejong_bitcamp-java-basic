640319551981b3b5e103c6215160882cf8dbc0db
sejong7007 <psj7007@gmail.com>
2019-08-12 11:46:00 +0900

    20190812 11:45

--- java basic/WhereLive.java
+++ java basic/WhereLive.java
@@ -3,8 +3,8 @@ class WhereLive {
     public static void main (String[] arge){
         System.out.println ("어디에 사니?"); 
         Scanner scanner = new Scanner (System.in);
-        String live =  "" ;
-        live = scanner.next() ;
-        System.out.println ("내가 사는 곳은 " + live + "이야");
+        String resion =  "" ;
+        resion = scanner.next() ;
+        System.out.println ("내가 사는 곳은 " + resion + "이야");
     }   
 }

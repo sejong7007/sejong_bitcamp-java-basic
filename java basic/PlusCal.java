8691300fad06942f04534edccbd63a117ad674b5
sejong7007 <psj7007@gmail.com>
2019-08-12 12:40:12 +0900

    20190812 12:40

--- java basic/PlusCal.java
+++ java basic/PlusCal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner ; 
-class SumTwoNumber {
+class PlusCal {
     public static void main (String[] args) {
         Scanner scanner = new Scanner (System.in) ;
         System.out.println ("더하고하 하는 첫 번째 수는?") ;
@@ -8,13 +8,8 @@ class SumTwoNumber {
         System.out.println ("더하고자 하는 두 번째 수는?") ;
         int b = 0 ;
         b = scanner.nextInt () ;
-        System.out.println ( a + "  +  " + b + "  =  " + (a+b));
-        System.out.println ("빼고자 하는 첫 번째 수는?") ;
         int c = 0 ;
-        c = scanner.nextInt () ;
-        System.out.println ("빼고자 하는 두 번째 수는?") ; 
-        int d = 0 ;
-        d = scanner.nextInt () ;
-        System.out.println (c + "  -  " + d + "  =  " +(c-d)) ; 
+        c = a + b ; 
+        System.out.println ( a + "  +  " + b + "  =  " + c );
     }
 }
